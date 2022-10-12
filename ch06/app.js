67e2e749e94c3c55968757c83ba5bd6d16e9e35a
김영훈 <joseph0926@naver.com>
2022-10-12 11:05:16 +0900

    2022.10.12

--- ch06/app.js
+++ ch06/app.js
@@ -5,23 +5,24 @@ const path = require("path");
 
 const app = express();
 
-const adminRoutes = require("./routes/admin");
+app.set("view engine", "ejs");
+
+const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", (req, res, next) => {
-  console.log("모든곳에 적용되는 미들웨어");
   next();
 });
 
-app.use("/admin", adminRoutes);
+app.use("/admin", adminData.routes);
 
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+  res.status(404).render("404", {pageTitle: "Page Not Found!"});
 });
 
 const server = http.createServer(app);
