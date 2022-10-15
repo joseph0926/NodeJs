cbe8156565db8593a501ea5bf91a5498e99968e1
김영훈 <joseph0926@naver.com>
2022-10-15 16:15:57 +0900

    2022.10.15

--- ch09/models/product.js
+++ ch09/models/product.js
@@ -15,20 +15,45 @@ const getProductsFromFile = (cb) => {
 }
 
 module.exports = class Product {
-  constructor(title) {
+  constructor(id, title, imageUrl, description, price) {
+    this.id = id;
     this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
   }
 
   save() {
     getProductsFromFile((products) => {
-      products.push(this);
-      fs.writeFile(p, JSON.stringify(products), (err) => {
-        console.log(err);
-      });
+      if (this.id) {
+        const existingProductIndex = products.findIndex((product) => {
+          return product.id === this.id
+        })
+        const updatedProduct = [...products];
+        updatedProduct[existingProductIndex] = this;
+        fs.writeFile(p, JSON.stringify(updatedProduct), (err) => {
+          console.log(err);
+        });
+      } else {
+        this.id = Math.random().toString();
+        products.push(this);
+        fs.writeFile(p, JSON.stringify(products), (err) => {
+          console.log(err);
+        });
+      }
     });
   }
 
   static fetchAll(cb) {
     getProductsFromFile(cb);
+  }
+
+  static findById(id, cb) {
+    getProductsFromFile((products) => {
+      const product = products.find((p) => {
+        return p.id === id
+      });
+      cb(product);
+    })
   }
 }
